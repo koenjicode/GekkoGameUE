ac87c80c13e042e1c1a02f3a2fd0208cb129efcd
Jordan Addams <43590463+koenjicode@users.noreply.github.com>
2026-05-03 17:02:41 +0100

    Added GekkoBlueprintFunctionLibrary

---
+++ Source/GekkoGameUE/Private/GekkoBlueprintFunctionLibrary.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GekkoBlueprintFunctionLibrary.h"
+
+int32 UGekkoBlueprintFunctionLibrary::GetPlayInEditorID()
+{
+	return UE::GetPlayInEditorID();
+}
---
+++ Source/GekkoGameUE/Public/GekkoBlueprintFunctionLibrary.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "GekkoBlueprintFunctionLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GEKKOGAMEUE_API UGekkoBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+public:
+	
+	UFUNCTION(BlueprintPure)
+	static int32 GetPlayInEditorID();
+};
