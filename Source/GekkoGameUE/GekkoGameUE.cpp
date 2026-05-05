ff4692e71194ba808d9c387765e8e86733556da5
Jordan Addams <43590463+koenjicode@users.noreply.github.com>
2026-05-05 20:31:03 +0100

    Log Fix + Rename

--- Source/GekkoGameUE/GekkoGameUE.cpp
+++ Source/GekkoGameUE/GekkoGameUE.cpp
@@ -1,6 +1,9 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "GekkoGameUE.h"
+#include "GekkoGameLog.h"
 #include "Modules/ModuleManager.h"
+
+DEFINE_LOG_CATEGORY(LogGekkoGame);
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GekkoGameUE, "GekkoGameUE" );
