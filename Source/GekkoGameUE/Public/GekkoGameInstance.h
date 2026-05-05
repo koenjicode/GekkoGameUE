74d32e74d22b0e23c01052f089e51a895908ddaf
Jordan Addams <43590463+koenjicode@users.noreply.github.com>
2026-05-05 20:34:06 +0100

    Removed Unused Entries

--- Source/GekkoGameUE/Public/GekkoGameInstance.h
+++ Source/GekkoGameUE/Public/GekkoGameInstance.h
@@ -6,18 +6,6 @@
 #include "Engine/GameInstance.h"
 #include "GekkoGameInstance.generated.h"
 
-USTRUCT(BlueprintType)
-struct FGekkoRemoteAddress
-{
-	GENERATED_BODY()
-	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	FString Address;
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	int32 Port;
-	
-};
-
 UCLASS()
 class GEKKOGAMEUE_API UGekkoGameInstance : public UGameInstance
 {
@@ -27,10 +15,6 @@ class GEKKOGAMEUE_API UGekkoGameInstance : public UGameInstance
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	bool bLocalPlayEnabled = false;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	TArray<FGekkoRemoteAddress> RemoteAddresses;
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	int32 LocalPort = 7000;
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	int32 PlayerId = -1;
 };
