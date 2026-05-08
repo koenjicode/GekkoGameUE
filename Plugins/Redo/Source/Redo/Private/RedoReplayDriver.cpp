20475bbf895e11da63e04e4ff8f3a85d0bf27837
Jordan Addams <43590463+koenjicode@users.noreply.github.com>
2026-05-08 16:50:15 +0100

    Redo Plugin Start

---
+++ Plugins/Redo/Source/Redo/Private/RedoReplayDriver.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RedoReplayDriver.h"
+
+
+// Sets default values
+ARedoReplayDriver::ARedoReplayDriver()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+// Called when the game starts or when spawned
+void ARedoReplayDriver::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARedoReplayDriver::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Plugins/Redo/Source/Redo/Public/RedoReplayDriver.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RedoReplayDriver.generated.h"
+
+UENUM()
+enum class ERedoReplayState : uint8
+{
+	Recording,
+	Playback,
+};
+
+UCLASS()
+class REDO_API ARedoReplayDriver : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ARedoReplayDriver();
+	
+	void InitReplayDriver();
+	
+	void RecordFrame();
+	void PlayFrame();
+	void RewindFrame();
+	
+private:
+	// stored in the replay file
+	
+	int32 InputSize;
+	
+	// allocated during playback
+	
+	ERedoReplayState ReplayState;
+	int32 SnapshotsPerFrame;
+	int32 StateSize;
+};
